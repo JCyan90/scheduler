b8d86c04bffa86091516935deb058a7df6b6ac90
Julien Atanassian <julienatanassian@MacBook-Pro-de-Julien.local>
2019-09-30 17:41:23 -0400

    refactor the interviewer components and made interviewerlist a controlled list

--- src/components/InterviewList.js
+++ src/components/InterviewList.js
@@ -10,11 +10,10 @@ export default function InterviewerList(props) {
       <ul className="interviewers__list">{props.interviewers.map(interviewer => 
           <InterviewerListItem
             key={interviewer.id}
-            id={interviewer.id}
             name={interviewer.name}
             avatar={interviewer.avatar}
-            selected={interviewer.id === props.interviewer}
-            setInterviewer={props.setInterviewer}
+            selected={interviewer.id === props.value}
+            setInterviewer={() => props.onChange(interviewer.id)}
           />
       )}
       </ul>
