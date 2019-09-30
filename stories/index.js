ac89cbae1bae5d04d3eeac411f3cb421169e3593
Julien Atanassian <julienatanassian@MacBook-Pro-de-Julien.local>
2019-09-30 18:37:57 -0400

    create the appointment/show component

--- stories/index.js
+++ stories/index.js
@@ -14,6 +14,7 @@ import InterviewerList from "components/InterviewList";
 import Appointment from "components/Appointment";
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
+import Show from "components/Appointment/Show";
 
 storiesOf("Button", module)
   .addParameters({
@@ -145,3 +146,11 @@ storiesOf("Appointment", module)
   ))
   .add("Header", () => <Header time="12pm" />)
   .add("Empty", () => <Empty onAdd={action("onAdd")} />)
+  .add("Show", () => (
+    <Show  
+      student="Lydia Miller-Jones"
+      interviewer={interviewer}
+      onEdit={action("onEdit")}
+      onDelete={action("onDelete")}
+    />
+  ))
