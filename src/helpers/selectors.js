5b261a73c04af2d01e9242c54fbad6f27034d14c
Julien Atanassian <julienatanassian@MacBook-Pro-de-Julien.local>
2019-10-02 17:24:02 -0400

    create and test getInterview selector

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,4 +1,4 @@
-export default function getAppointmentsForDay(state, day) {
+function getAppointmentsForDay(state, day) {
   const result = [];
   const dayData = state.days.filter(d => d.name === day)
   if (!dayData[0]) return result;
@@ -7,8 +7,20 @@ export default function getAppointmentsForDay(state, day) {
   }
   return result;
 
+  // Showed by a mentor but to uncomfortable with reduce to use/understand it
   // if (!state) return []
   // const dayData = state.days.filter(d => d.name === day)
   // if (dayData.length === 0) return [];
   // return dayData[0].appointments.reduce((cum, cur) => cum.concat(state.appointments[cur]), [])
 };
+
+function getInterview(state, interview) {
+  if (interview) {
+    const interviewer = state.interviewers[interview.interviewer];
+    // const interviewerData = state.interviewers.filter(i => i.id === interview.interviewer)
+    return { ...interview, interviewer };
+  }
+  return null;
+}
+
+export { getAppointmentsForDay, getInterview };
